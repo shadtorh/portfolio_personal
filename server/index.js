d2427a70f439bc7c9a17dd3582ca2886a56693b4
shadtorh <torhshad09@gmail.com>
2025-04-29 00:19:28 -0400

    cleaning my codes v6

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ const transporter = nodemailer.createTransport({
 	},
 });
 
+app.get("/", (req, res) => {
+	res.send("Hello from the server!");
+});
+
 app.post("/send-email", async (req, res) => {
 	const { name, email, message } = req.body;
 
